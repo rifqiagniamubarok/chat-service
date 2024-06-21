04be9b23e5456a5e06373d11435a5296b3c36511
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2024-06-21 11:16:37 +0700

    initiate

---
+++ app.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+
+const app = express();
+const httpServer = createServer(app);
+
+app.use(cors());
+
+const io = new Server(httpServer, {
+  /* options */
+  cors: {
+    origin: 'http://localhost:3000',
+  },
+});
+
+io.on('connection', (socket) => {
+  let { id, handshake, rooms, data } = socket;
+  socket.on('send_message', (data) => {
+    io.to(data.room).emit('receive_message', data);
+  });
+  socket.on('join_room', (data) => {
+    socket.join(data);
+  });
+});
+
+httpServer.listen(3550, () => {
+  console.log('running on port 3550');
+});
